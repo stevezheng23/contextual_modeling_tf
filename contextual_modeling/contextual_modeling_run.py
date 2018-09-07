e91620f3535b43f86843ebf2d9561bb51da91146
Mingzhi Zheng <mizheng@microsoft.com>
2018-09-07 09:00:23 -0700

    add contextual modeling framework with trainig enabled & implementation for deep attention matching model

---
+++ contextual_modeling/contextual_modeling_run.py
@@ -0,0 +1,103 @@
+import argparse
+import os.path
+import time
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.python import debug as tf_debug
+
+from util.default_util import *
+from util.param_util import *
+from util.model_util import *
+from util.eval_util import *
+from util.debug_logger import *
+from util.train_logger import *
+from util.eval_logger import *
+from util.summary_writer import *
+
+def add_arguments(parser):
+    parser.add_argument("--mode", help="mode to run", required=True)
+    parser.add_argument("--config", help="path to json config", required=True)
+
+def train(logger,
+          hyperparams,
+          enable_eval=True,
+          enable_debug=False):
+    config_proto = get_config_proto(hyperparams.device_log_device_placement,
+        hyperparams.device_allow_soft_placement, hyperparams.device_allow_growth,
+        hyperparams.device_per_process_gpu_memory_fraction)
+    
+    summary_output_dir = hyperparams.train_summary_output_dir
+    if not tf.gfile.Exists(summary_output_dir):
+        tf.gfile.MakeDirs(summary_output_dir)
+    
+    logger.log_print("##### create train model #####")
+    train_model = create_train_model(logger, hyperparams)
+    train_sess = tf.Session(config=config_proto, graph=train_model.graph)
+    if enable_debug == True:
+        train_sess = tf_debug.LocalCLIDebugWrapperSession(train_sess)
+    
+    train_summary_writer = SummaryWriter(train_model.graph, os.path.join(summary_output_dir, "train"))
+    init_model(train_sess, train_model)
+    train_logger = TrainLogger(hyperparams.data_log_output_dir)
+    
+    if enable_eval == True:
+        logger.log_print("##### create infer model #####")
+        infer_model = create_infer_model(logger, hyperparams)
+        infer_sess = tf.Session(config=config_proto, graph=infer_model.graph)
+        if enable_debug == True:
+            infer_sess = tf_debug.LocalCLIDebugWrapperSession(infer_sess)
+        
+        infer_summary_writer = SummaryWriter(infer_model.graph, os.path.join(summary_output_dir, "infer"))
+        init_model(infer_sess, infer_model)
+        eval_logger = EvalLogger(hyperparams.data_log_output_dir)
+    
+    logger.log_print("##### start training #####")
+    global_step = 0
+    train_model.model.save(train_sess, global_step, "debug")
+    for epoch in range(hyperparams.train_num_epoch):
+        train_sess.run(train_model.data_pipeline.initializer)
+        step_in_epoch = 0
+        while True:
+            try:
+                start_time = time.time()
+                train_result = train_model.model.train(train_sess, train_model.word_embedding)
+                end_time = time.time()
+                
+                global_step = train_result.global_step
+                step_in_epoch += 1
+                train_logger.update(train_result, epoch, step_in_epoch, end_time-start_time)
+                
+                if step_in_epoch % hyperparams.train_step_per_stat == 0:
+                    train_logger.check()
+                    train_summary_writer.add_summary(train_result.summary, global_step)
+                if step_in_epoch % hyperparams.train_step_per_ckpt == 0:
+                    train_model.model.save(train_sess, global_step, "debug")
+            except tf.errors.OutOfRangeError:
+                train_logger.check()
+                train_summary_writer.add_summary(train_result.summary, global_step)
+                train_model.model.save(train_sess, global_step, "epoch")
+                break
+
+    train_summary_writer.close_writer()
+    if enable_eval == True:
+        infer_summary_writer.close_writer()
+    
+    logger.log_print("##### finish training #####")
+
+def main(args):
+    hyperparams = load_hyperparams(args.config)
+    logger = DebugLogger(hyperparams.data_log_output_dir)
+    
+    tf_version = check_tensorflow_version()
+    logger.log_print("# tensorflow verison is {0}".format(tf_version))
+    
+    if (args.mode == 'train'):
+        train(logger, hyperparams, enable_eval=False, enable_debug=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    add_arguments(parser)
+    args = parser.parse_args()
+    main(args)
