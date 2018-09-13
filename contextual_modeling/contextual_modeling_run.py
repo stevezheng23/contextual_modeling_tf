42a8df553d515db8b30216cad713e5e69ad39b68
Mingzhi Zheng <mizheng@microsoft.com>
2018-09-12 21:47:48 -0700

    add evaluate method for contextual modeling

--- contextual_modeling/contextual_modeling_run.py
+++ contextual_modeling/contextual_modeling_run.py
@@ -74,10 +74,24 @@ def train(logger,
                     train_summary_writer.add_summary(train_result.summary, global_step)
                 if step_in_epoch % hyperparams.train_step_per_ckpt == 0:
                     train_model.model.save(train_sess, global_step, "debug")
+                if step_in_epoch % hyperparams.train_step_per_eval == 0 and enable_eval == True:
+                    ckpt_file = infer_model.model.get_latest_ckpt("debug")
+                    extrinsic_eval(eval_logger, infer_summary_writer, infer_sess,
+                        infer_model, infer_model.input_data, infer_model.input_question,
+                        infer_model.input_context, infer_model.input_answer, infer_model.word_embedding,
+                        hyperparams.train_eval_batch_size, hyperparams.train_eval_metric,
+                        hyperparams.train_eval_detail_type, global_step, epoch, ckpt_file, "debug")
             except tf.errors.OutOfRangeError:
                 train_logger.check()
                 train_summary_writer.add_summary(train_result.summary, global_step)
                 train_model.model.save(train_sess, global_step, "epoch")
+                if enable_eval == True:
+                    ckpt_file = infer_model.model.get_latest_ckpt("epoch")
+                    extrinsic_eval(eval_logger, infer_summary_writer, infer_sess,
+                        infer_model, infer_model.input_data, infer_model.input_question,
+                        infer_model.input_context, infer_model.input_answer, infer_model.word_embedding,
+                        hyperparams.train_eval_batch_size, hyperparams.train_eval_metric,
+                        hyperparams.train_eval_detail_type, global_step, epoch, ckpt_file, "epoch")
                 break
 
     train_summary_writer.close_writer()
@@ -85,6 +99,41 @@ def train(logger,
         infer_summary_writer.close_writer()
     
     logger.log_print("##### finish training #####")
+
+def evaluate(logger,
+             hyperparams,
+             enable_debug=False):   
+    config_proto = get_config_proto(hyperparams.device_log_device_placement,
+        hyperparams.device_allow_soft_placement, hyperparams.device_allow_growth,
+        hyperparams.device_per_process_gpu_memory_fraction)
+    
+    summary_output_dir = hyperparams.train_summary_output_dir
+    if not tf.gfile.Exists(summary_output_dir):
+        tf.gfile.MakeDirs(summary_output_dir)
+    
+    logger.log_print("##### create infer model #####")
+    infer_model = create_infer_model(logger, hyperparams)
+    infer_sess = tf.Session(config=config_proto, graph=infer_model.graph)
+    if enable_debug == True:
+        infer_sess = tf_debug.LocalCLIDebugWrapperSession(infer_sess)
+    
+    infer_summary_writer = SummaryWriter(infer_model.graph, os.path.join(summary_output_dir, "infer"))
+    init_model(infer_sess, infer_model)
+    eval_logger = EvalLogger(hyperparams.data_log_output_dir)
+    
+    logger.log_print("##### start evaluation #####")
+    global_step = 0
+    eval_mode = "debug" if enable_debug == True else "epoch"
+    ckpt_file_list = infer_model.model.get_ckpt_list(eval_mode)
+    for i, ckpt_file in enumerate(ckpt_file_list):
+        extrinsic_eval(eval_logger, infer_summary_writer, infer_sess,
+            infer_model, infer_model.input_data, infer_model.input_question,
+            infer_model.input_context, infer_model.input_answer, infer_model.word_embedding,
+            hyperparams.train_eval_batch_size, hyperparams.train_eval_metric,
+            hyperparams.train_eval_detail_type, global_step, i, ckpt_file, eval_mode)
+    
+    infer_summary_writer.close_writer()
+    logger.log_print("##### finish evaluation #####")
 
 def main(args):
     hyperparams = load_hyperparams(args.config)
